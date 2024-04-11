f7ce24e10b18e3665c852e6e687dc9329813542b
kriptonian1 <shymal.sawan@gmail.com>
2024-04-11 21:28:20 +0530

    feat: Update command aliases and add new commands for configuring, adding, removing, and listing projects and workspaces

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -11,12 +11,29 @@ pub fn cli() -> Command {
         .arg_required_else_help(true)
         .subcommand(
             Command::new("configure")
-                .alias("config")
-                .about("Configure the keyshades CLI, alias to `config`")
+                .alias("conf")
+                .about("Configure the keyshades CLI, alias to `conf`")
                 .arg(arg!(-w --workspace <WORKSPACE> "Configure the workspace"))
                 .arg(arg!(-p --project <PROJECT> "Configure the project")),
         )
-        .subcommand(Command::new(""))
+        .subcommand(
+            Command::new("add")
+                .about("Add a new project to the workspace or a new workspace")
+                .arg(arg!(-w --workspace <WORKSPACE> "Add the workspace").required(true))
+                .arg(arg!(-p --project <PROJECT> "Add the project, to add a project you must specify the workspace")),
+        ).subcommand(
+            Command::new("remove")
+                .alias("rm")
+                .about("Remove project(s) or workspace(s), alias to `rm`")
+                .arg(arg!(-w --workspace <WORKSPACE> "Configure the workspace ").required(true))
+                .arg(arg!(-p --project <PROJECT> "Configure the project")),
+        ).subcommand(
+            Command::new("list")
+                .alias("li")
+                .about("Add project(s) or workspace(s), alias to `li`")
+                .arg(arg!(-w --workspace <WORKSPACE> "list all workspace").required(true))
+                .arg(arg!(-p --project <PROJECT> "list all projects")),
+        )
 }
 /// Execute the command line application
 pub fn main() {
